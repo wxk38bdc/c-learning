c9d46ba878b030cf9cd867ead2fc15f137bb4a98
wxk38bdc <2227814847@qq.com>
2023-10-19 07:32:34 +0800

    daily submission 20231019

--- Project_20231016/c_test.c
+++ Project_20231016/c_test.c
@@ -112,10 +112,228 @@
 //	return 0;
 //}
 
-#include <stdio.h>
-#include <string.h>
-int main()
-{
+//int is_prime(int n)
+//{
+//	int i = 0;
+//	for (i = 2; i < n; i++)
+//	{
+//		if (n % i == 0)
+//		{
+//			return 0;
+//		}
+//	}
+//	return 1;
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int count = 0;
+//	for (int i = 5; i <=n; i+=2)
+//	{
+//		if (is_prime(i) == 1 && is_prime(i - 2) == 1)
+//		{
+//			printf("%d %d\n", i - 2, i);
+//			count++;
+//		}
+//	}
+//	if(count == 0)
+//	{
+//		printf("empty\n");
+//	}
+//	return 0;
+//}
 
-	return 0;
-}
+//先缓一缓
+//#include <stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	char arr[210] = { 0 };
+//	//输入字符串
+//	scanf("%[^\n]", arr);
+//	//获取字符串长度
+//	int len = strlen(arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <string.h>
+//
+//void replaceSubstring(char* original, const char* find, const char* replace) {
+//    char newString[1000]; // 适当分配足够大的缓冲区
+//    char* start = original;
+//    while ((start = strstr(start, find))) {
+//        strncpy(newString, original, start - original); // 复制前半部分
+//        newString[start - original] = '\0'; // 在新字符串末尾添加 null 终止符
+//        strcat(newString, replace); // 添加替换字符串
+//        strcat(newString, start + strlen(find)); // 添加剩余部分
+//        strcpy(original, newString); // 将新字符串复制回原始字符串
+//        start += strlen(replace); // 移动到下一个匹配位置
+//    }
+//}
+//
+//int main() {
+//    char str[] = "This is a test. This is only a test.";
+//    const char* find = "is";
+//    const char* replace = "was";
+//    replaceSubstring(str, find, replace);
+//    printf("%s\n", str);
+//    return 0;
+//}
+
+//int is_prime(int n)
+//{
+//	int i = 0;
+//	for (i = 2; i < n; i++)
+//	{
+//		if (n % i == 0)
+//		{
+//			return 0;
+//		}
+//	}
+//	return 1;
+//}
+//int reverse_num(int n)
+//{
+//	int tmp = 0;
+//	while (n)
+//	{
+//		tmp = tmp * 10 + n % 10;
+//		n /= 10;
+//	}
+//	return tmp;
+//}
+//int main()
+//{
+//	int m = 0, n = 0, count = 0, count_num[100000] = { 0 };
+//	scanf("%d %d", &m, &n);
+//	for (int i = m; i <= n; i++)
+//	{
+//		if (is_prime(i) == 1&&is_prime(reverse_num(i))==1)
+//		{
+//			count_num[count] = i;
+//			count++;
+//		}
+//	}
+//	for (int i = 0; i <count-1; i++)
+//	{
+//		printf("%d,", count_num[i]);
+//	}
+//	if (count != 0)
+//	{
+//		printf("%d", count_num[count - 1]);
+//	}
+//	if(count==0)
+//	{
+//		printf("No");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 1, t = 0;
+//	scanf("%d", &t);
+//	int A = 0, B = 0;
+//	while (n <= t)
+//	{
+//		int arr[100] = { 0 };
+//		int count = 0;
+//		int i = 0;
+//		int count_1 = 0,count_0 = 0;
+//		int m = n;
+//		while (m)
+//		{
+//			arr[count] = m % 2;
+//			m /= 2;
+//			count++;
+//		}
+//		for (i = 0; i < count; i++)
+//		{
+//			if (arr[i] == 1)
+//			{
+//				count_1++;
+//			}
+//			else
+//			{
+//				count_0++;
+//			}
+//		}
+//		//printf("%d:%d %d\n", n, count_1, count_0);
+//		if (count_1 > count_0)
+//		{
+//			A++;
+//		}
+//		else
+//		{
+//			B++;
+//		}
+//		n++;
+//	}
+//	printf("%d %d", A, B);
+//	return 0;
+//}
+
+//#include<stdio.h>
+////用递归算法将一个十进制数转换为任意进制数M(M<=16)
+////Thanks GItHub copilot!!!I love you!!!
+//void convert(int n, int m)
+//{
+//	if (n>0&&m<=10)
+//	{
+//		convert(n / m, m);
+//		printf("%d", n % m);
+//	}
+//	else if (n>0&&m>10)
+//	{
+//		convert(n / m, m);
+//		if (n % m >= 10)
+//		{
+//			printf("%c", n % m - 10 + 'A');
+//		}
+//		else
+//		{
+//			printf("%d", n % m);
+//		}
+//	}
+//}
+//int main()
+//{
+//	int n, m;
+//	scanf("%d %d", &n, &m);
+//	convert(n, m);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	char arr1[10] = { 0 };
+//	char arr2[10] = { 0 };
+//	scanf("%s %s", arr1, arr2);
+//	int i = 0, j = 0;
+//	int len1 = strlen(arr1);
+//	int len2 = strlen(arr2);
+//	int ret1 = 1, ret2 = 1;
+//	for (i = 0; i < len1; i++)
+//	{
+//		ret1*=arr1[i]-'A'+1;
+//	}
+//	for (j = 0; j < len2; j++)
+//	{
+//		ret2 *= arr2[j] - 'A' + 1;
+//	}
+//	if (ret1 % 47 == ret2 % 47)
+//	{
+//		printf("GO");
+//	}
+//	else
+//	{
+//		printf("STAY");
+//	}
+//	return 0;
+//}
+
+//#include<stdio.h>
--- Project_20231016/test.cpp
+++ Project_20231016/test.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 
 //#include <iostream>
 //using namespace std;
@@ -14,3 +15,96 @@
 //	}
 //	cout << sum;
 //}
+
+////这题不这样写简直没办法
+//int main() 
+//{
+//	string arr[105];
+//	string s;
+//	int n = 0;
+//	while (cin >> s)
+//	{
+//		n++;
+//		arr[n] = s;
+//	}
+//	//You 0 want 1 someone 2 to 3 help 4 you 5
+//	//You 6
+//	//I 7
+//	for (int i = 1; i <= n - 2; i++) 
+//	{//注意，读入的时候将想被替换的单词放在了 n-1 的位置，替换的单词放在了 n 的位置，所以循环一直到 n-2 即可。
+//		if (arr[i] == arr[n - 1])
+//		{
+//			cout << arr[n] << " ";//判断到要被替换的单词就输出替换它的单词。
+//		}
+//		else
+//		{
+//			cout << arr[i] << " ";
+//		}
+//	}
+//	return 0;
+//}
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//#include <iostream>
+//#include <cstring>
+//using namespace std;
+//int main() {
+//    char a[5];
+//    long long i[5];
+//    char b[5];
+//    long long j[5];
+//    cin >> a >> b;
+//    long long x = 1, y = 1;
+//    for (int m = 0; m < strlen(a); m++) {
+//
+//        i[m] = int(a[m]) - 64;
+//        x = i[m] * x;
+//
+//    }
+//    for (int n = 0; n < strlen(b); n++) {
+//        j[n] = int(b[n]) - 64;
+//        y = j[n] * y;
+//    }
+//
+//    if ((x % 47) == (y % 47))
+//        cout << "GO";
+//    else
+//        cout << "STAY";
+//    return 0;
+//}
+
+//
+//int main() {
+//	char a[100] = { 0 };
+//	int i[100] = { 0 };
+//	char b[100] = { 0 };
+//	int j[100] = { 0 };
+//	scanf("%s %s", a, b);
+//	int x = 1, y = 1;
+//	for (int m = 0; m < strlen(a); m++) {
+//
+//		i[m] = a[m] - 64;
+//		x = i[m] * x;
+//		cout << i[m] << endl;
+//	}
+//	for (int n = 0; n < strlen(b); n++) {
+//		j[n] = (b[n]) - 64;
+//		y = j[n] * y;
+//		cout << j[n] << endl;
+//	}
+//
+//	if ((x % 47) == (y % 47)) {
+//		cout << "GO";
+//	}
+//	else {
+//		cout << "STAY";
+//	}
+//	return 0;
+//}
+
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+using namespace std;
